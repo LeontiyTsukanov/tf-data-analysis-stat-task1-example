43a4916b24448c70131357753a390f2efcfae049
Leontiy Tsukanov <122561265+LeontiyTsukanov@users.noreply.github.com>
2023-04-05 20:56:28 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,9 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 407415686 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    const = np.array([335 for _ in range(len(x))])
+    x = x - const
+    return np.log(x).mean()
